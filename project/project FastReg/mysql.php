9f83417fdf090bbf747c849de2d1fdee7f61619f
Franzi <115472701+xginevra@users.noreply.github.com>
2023-11-22 12:11:59 +0100

    update

---
+++ project/project FastReg/mysql.php
@@ -0,0 +1,8 @@
+<?php
+    $username = "root";
+    $password = "";
+
+    $db = mysqli_connect("localhost", $username, $password) or die("Connection failed!");
+    mysqli_select_db("fastregdb", $db);
+?>
+
