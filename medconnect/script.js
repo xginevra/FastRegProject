b49f2fb411972875cdf677fec074b53b3ed27a67
thisispaniz <paniz.ziaie@gmail.com>
2023-12-11 18:36:52 +0100

    show/hide toggle

---
+++ medconnect/script.js
@@ -0,0 +1,9 @@
+let showPasswordBtn = document.querySelector('.show-password');
+let passwordInp = document.querySelector('.password-input');
+
+showPasswordBtn.addEventListener('click', () => {
+    showPasswordBtn.classList.toggle('fa-eye');
+    showPasswordBtn.classList.toggle('fa-eye-slash');
+
+    passwordInp.type = (passwordInp.type === 'password') ? 'text' : 'password';
+});
