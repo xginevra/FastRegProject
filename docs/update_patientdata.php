6217a6d00b790683e2994dee5420f221fa60ec15
Franzi <115472701+xginevra@users.noreply.github.com>
2024-01-10 22:31:41 +0100

    updated database information

--- docs/update_patientdata.php
+++ docs/update_patientdata.php
@@ -3,10 +3,17 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'medconnect';
+// $DATABASE_HOST = 'localhost';
+// $DATABASE_USER = 'root';
+// $DATABASE_PASS = '';
+// $DATABASE_NAME = 'medconnect';
+
+
+$DATABASE_HOST = 'rdbms.strato.de';
+$DATABASE_USER = 'dbu123640';
+$DATABASE_PASS = 'MouzHIwS23/24paN';
+$DATABASE_NAME = 'dbs12338865';
+
 
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
